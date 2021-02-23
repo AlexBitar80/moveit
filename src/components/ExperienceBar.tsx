dd5e4a53febfe85b679b5b29b190fde3d3cde86e
Alexandre Bitar <joabitar76@gmail.com>
2021-02-23 09:55:08 -0300

    chore: added specific styles to the experience bar

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,11 +1,13 @@
+import styles from '../styles/components/ExperienceBar.module.css';
+
 export function ExperienceBar() {
   return (
-    <header className="experience-bar">
+    <header className={styles.experienceBar}>
       <span>0 xp</span>
       <div>
         <div style={{ width: '50%' }} />
 
-        <span style={{ left: '50%' }} className="current-experience">
+        <span style={{ left: '50%' }} className={styles.currentExperience}>
           300px
         </span>
       </div>
