82b14d576e7b8880c3c14520ebbf57f37a199d34
Alexandre Bitar <joabitar76@gmail.com>
2021-02-23 14:30:47 -0300

    chore: application logo added to the browser header

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,9 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link></link>
+
+          <link rel="shortout icon" href="favicon.png" type="image/png" />
           <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap"
             rel="stylesheet"
