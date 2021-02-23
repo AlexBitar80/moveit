20388e331870c2f406b0a5bdc8f73498a81d7d73
Alexandre Bitar <joabitar76@gmail.com>
2021-02-23 11:44:31 -0300

    chore: added component of complete challenges

---
+++ src/components/CompleteChallenge.tsx
@@ -0,0 +1,10 @@
+import style from '../styles/components/completeChallenge.module.css';
+
+export default function CompleteChallenge() {
+  return (
+    <div className={style.completeChallengeContainer}>
+      <span>Desafios completos</span>
+      <span>5</span>
+    </div>
+  );
+}
