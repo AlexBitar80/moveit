8f41b7c0a6925521f335b7a0efda7de01a221b8e
Alexandre Bitar <joabitar76@gmail.com>
2021-02-24 12:44:21 -0300

    chore: added challenge box component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
+import Head from 'next/head';
 import CompleteChallenge from '../components/CompleteChallenge';
 import { Countdown } from '../components/Countdown';
 import { ExperienceBar } from '../components/ExperienceBar';
 import { Profile } from '../components/Profile';
-
-import Head from 'next/head';
+import { ChallengeBox } from '../components/ChallengeBox';
 
 import styles from '../styles/pages/Home.module.css';
 
@@ -22,7 +22,9 @@ export default function Home() {
           <CompleteChallenge />
           <Countdown />
         </div>
-        <div></div>
+        <div>
+          <ChallengeBox />
+        </div>
       </section>
     </div>
   );
