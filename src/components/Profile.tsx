70cfa0bb5523ad20191557e7808afdb11bc8a582
Alexandre Bitar <joabitar76@gmail.com>
2021-02-25 11:20:30 -0300

    chore: added level changes from context

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../context/ChallengesContext';
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+  const { level } = useContext(ChallengesContext);
+
   return (
     <div className={styles.profileContainer}>
       <img src="https://github.com/AlexBitar80.png" alt="João Alexandre" />
@@ -8,7 +12,7 @@ export function Profile() {
         <strong>João Alexandre</strong>
         <p>
           <img src="icons/level.svg" alt="level" />
-          Level 2
+          Level {level}
         </p>
       </div>
     </div>
