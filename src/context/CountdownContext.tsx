1ab12d90edfae076327d628a17d7c22b19a8914d
Alexandre Bitar <joabitar76@gmail.com>
2021-02-25 11:16:30 -0300

    chore: added context for countdown

---
+++ src/context/CountdownContext.tsx
@@ -0,0 +1,74 @@
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from 'react';
+import { ChallengesContext } from './ChallengesContext';
+
+interface CountdownContextData {
+  resetCountdonw: () => void;
+  startCountdonw: () => void;
+  minutes: number;
+  seconds: number;
+  hasFinished: boolean;
+  isActive: boolean;
+}
+
+interface CountdownContextProviderProps {
+  children: ReactNode;
+}
+
+export const CountdownContext = createContext({} as CountdownContextData);
+
+let countdownTimeout: NodeJS.Timeout;
+
+export function CountdownProvider({ children }: CountdownContextProviderProps) {
+  const { startNewChallenge } = useContext(ChallengesContext);
+
+  const [time, setTime] = useState(0.1 * 60);
+  const [isActive, setIsActive] = useState(false);
+  const [hasFinished, setHasFinished] = useState(false);
+
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+
+  function startCountdonw() {
+    setIsActive(true);
+  }
+
+  function resetCountdonw() {
+    clearTimeout(countdownTimeout);
+    setIsActive(false);
+    setHasFinished(false);
+    setTime(0.1 * 60);
+  }
+
+  useEffect(() => {
+    if (isActive && time > 0) {
+      countdownTimeout = setTimeout(() => {
+        setTime(time - 1);
+      }, 1000);
+    } else if (isActive && time === 0) {
+      setHasFinished(true);
+      setIsActive(false);
+      startNewChallenge();
+    }
+  }, [isActive, startNewChallenge, time]);
+
+  return (
+    <CountdownContext.Provider
+      value={{
+        resetCountdonw,
+        startCountdonw,
+        minutes,
+        seconds,
+        hasFinished,
+        isActive,
+      }}
+    >
+      {children}
+    </CountdownContext.Provider>
+  );
+}
