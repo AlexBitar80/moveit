7b77fee7d25ae153ec0a70c5f64c5170e5291043
Alexandre Bitar <joabitar76@gmail.com>
2021-02-26 13:29:08 -0300

    chore: moved challenge context function to another file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,7 @@
-import { ChallengesProvider } from '../context/ChallengesContext';
 import '../styles/global.css';
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <ChallengesProvider>
-      <Component {...pageProps} />
-    </ChallengesProvider>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
