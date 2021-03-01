34c6ff46de0fd464952ba864a7815fec5c85b0cb
Alexandre Bitar <joabitar76@gmail.com>
2021-03-01 14:49:20 -0300

    chore: added icon based on dark or light theme

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,16 @@
 import { useContext } from 'react';
 import { ThemeContext } from '../context/ThemeContext';
 import { Container, SwitchTheme } from '../styles/components/Header';
+import { RiSunLine } from 'react-icons/ri';
+import { BiMoon } from 'react-icons/bi';
 
 export function Header() {
   const { toggleTheme, theme } = useContext(ThemeContext);
 
   return (
     <Container>
+      {theme === 'light' ? <BiMoon size={20} /> : <RiSunLine size={20} />}
+
       <SwitchTheme
         checked={theme === 'light'}
         height={20}
