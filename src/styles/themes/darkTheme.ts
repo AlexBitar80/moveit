38b23f725b8989a8949271d026d70df417d7b9e1
Alexandre Bitar <joabitar76@gmail.com>
2021-03-01 11:45:32 -0300

    style: dark and light themes

---
+++ src/styles/themes/darkTheme.ts
@@ -0,0 +1,17 @@
+export default {
+  title: 'dark',
+
+  colors: {
+    box: '#6272a4',
+    background: '#282a36',
+    grayLine: '#dcdde0',
+    text: '#fff',
+    title: '#f8f8f2',
+    primaryRed: '#e83f5b',
+    redHover: '#ba181b',
+    primaryGreen: '#50fa7b',
+    greenHover: '#4cd62b',
+    blueDark: '#4953b8',
+    blueTwitter: '#2aa9e0',
+  },
+};
---
+++ src/styles/themes/lightTheme.ts
@@ -0,0 +1,17 @@
+export default {
+  title: 'light',
+
+  colors: {
+    box: '#fff',
+    background: '#f2f3f5',
+    grayLine: '#dcdde0',
+    text: '#666666',
+    title: '#2e384d',
+    primaryRed: '#e83f5b',
+    redHover: '#ba181b',
+    primaryGreen: '#50fa7b',
+    greenHover: '#4cd62b',
+    blueDark: '#4953b8',
+    blueTwitter: '#2aa9e0',
+  },
+};
