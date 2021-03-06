1f361e323e4eacae57af43d736e5d5a1f6135c01
Alexandre Bitar <joabitar76@gmail.com>
2021-03-06 16:20:27 -0300

    style: added styles to the home page

--- src/styles/pages/Home.ts
+++ src/styles/pages/Home.ts
@@ -13,7 +13,7 @@ export const Container = styled.div`
     flex: 1;
 
     display: grid;
-    grid-template-columns: 1fr 1fr;
+    grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
     gap: 6.25rem;
     align-content: center;
   }
